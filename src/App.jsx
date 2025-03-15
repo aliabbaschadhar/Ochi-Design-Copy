017a6566275a56d6ed4746015bf64c5a420e297a
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-03-15 17:40:04 +0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline flex items-center justify-center
+      h-screen bg-gradient-to-tr from-blue-400 via-blue-800 to-blue-900 text-slate-500">
+      Ochi Design
+    </h1>
+  )
+}
