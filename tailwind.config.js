8240eb92b34205e4d97a55f51324cf0cffdaccac
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-03-15 19:48:57 +0500

    configured tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'founders': ['FoundersGrotesk', 'sans-serif'],
+        'neue': ['NeueMontreal', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
