32c417fb730657d3d0017dc5ad3f334154c9fc68
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-03-16 23:58:43 +0500

    Marquee done

---
+++ src/components/Marquee.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Marquee() {
+    return (
+        <div
+            className='w-full h-[68vh] bg-[#004d43] rounded-t-3xl py-20'
+        >
+            <div className='border-y-2 border-zinc-300 flex whitespace-nowrap overflow-hidden py-3'>
+                {['We are ochi', 'We are ochi', 'We are Ochi', 'We are ochi', 'We are ochi', 'We are ochi'].map((text, index) => (
+                    <h1
+                        key={index}
+                        className='text-[20vw] leading-none font-founders uppercase mb-[1vw] pr-[2vw] text-white'
+                    >
+                        {text}
+                    </h1>
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Marquee;
