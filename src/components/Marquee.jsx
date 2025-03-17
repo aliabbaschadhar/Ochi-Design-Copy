c9e5a754a1556c903920e9516276f1204c5be4f4
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-03-17 07:59:14 +0500

    A small change marquee padding

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Marquee() {
     return (
         <div
-            className='w-full h-[68vh] bg-[#004d43] rounded-t-3xl py-20'
+            className='w-full h-[68vh] bg-[#004d43] rounded-t-3xl py-20 pb-4'
         >
             <div className='border-y-2 border-zinc-300 flex whitespace-nowrap overflow-hidden py-3'>
                 {['We are ochi', 'We are ochi', 'We are Ochi', 'We are ochi', 'We are ochi', 'We are ochi'].map((text, index) => (
