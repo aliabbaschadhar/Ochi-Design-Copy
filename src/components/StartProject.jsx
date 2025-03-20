3cf69f3bcf4feda30c9d806271de086dc6602fe2
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-03-20 23:02:23 +0500

    Start the Project component done

--- src/components/StartProject.jsx
+++ src/components/StartProject.jsx
@@ -3,7 +3,7 @@ import { Button, EyeBall } from "./"
 
 function StartProject() {
     return (
-        <section className='w-full bg-[#cdea68] p-[20vw]' >
+        <section className='w-full bg-[#cdea68] p-[18vw]' >
             <div className='w-full flex flex-col items-center justify-center relative'>
                 {["Ready to", "Start the", "Project ?"]
                     .map((item, index) => (
