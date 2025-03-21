4a808e7e4792c1444130a457fe3774ae1eb07ca0
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-03-21 06:57:07 +0500

    Locomotive init

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import {
   About,
   BeforeFooter,
@@ -11,21 +12,89 @@ import {
   StartProject
 }
   from "./components";
+import LocomotiveScroll from "locomotive-scroll";
 
 
 export default function App() {
+
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: document.querySelector("[data-scroll-container]"),
+      smooth: true,
+    });
+
+    return () => scroll.destroy();
+  }, []);
+
   return (
-    <main className="w-full h-screen ">
-      <Navbar />
-      <LandingPage />
-      <Marquee />
-      <About />
-      <Eyes />
-      <Featured />
-      <Reviews />
-      <BeforeFooter />
-      <StartProject />
-      <Footer />
+    <main
+      data-scroll-container
+      className="w-full min-h-screen "
+    >
+      <div data-scroll-section>
+        <Navbar />
+      </div>
+
+      <div
+        data-scroll-section
+      >
+        <div
+          data-scroll
+          data-scroll-speed="-.3"
+        >
+          <LandingPage />
+        </div>
+      </div>
+      <div
+        data-scroll-section
+      >
+        <div
+          data-scroll
+          data-scroll-speed='-.1'
+        >
+          <Marquee />
+        </div>
+      </div>
+      <div
+        data-scroll-section
+      >
+        <div
+          data-scroll
+          data-scroll-speed='-.02'
+        >
+          <About />
+        </div>
+      </div>
+      <div
+        data-scroll-section
+      >
+        <Eyes />
+      </div>
+      <div
+        data-scroll-section
+      >
+        <Featured />
+      </div>
+      <div
+        data-scroll-section
+      >
+        <Reviews />
+      </div>
+      <div
+        data-scroll-section
+      >
+        <BeforeFooter />
+      </div>
+      <div
+        data-scroll-section
+      >
+        <StartProject />
+      </div>
+      <div
+        data-scroll-section
+      >
+        <Footer />
+      </div>
     </main>
   )
 }
