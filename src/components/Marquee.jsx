c7c7ef4fba680148a034a83fca4f0e8c60ce6111
aliabbaschadhar <work.aliabbaschadhar@gmail.com>
2025-03-21 07:00:55 +0500

    Locomotive and animation done

--- src/components/Marquee.jsx
+++ src/components/Marquee.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from 'framer-motion';
 
 function Marquee() {
     return (
@@ -7,12 +8,15 @@ function Marquee() {
         >
             <div className='border-y-2 border-zinc-300 flex whitespace-nowrap overflow-hidden py-3'>
                 {['We are ochi', 'We are ochi', 'We are Ochi', 'We are ochi', 'We are ochi', 'We are ochi'].map((text, index) => (
-                    <h1
+                    <motion.h1
                         key={index}
                         className='text-[20vw] leading-none font-founders uppercase mb-[1vw] pr-[2vw] text-white'
+                        initial={{ x: 0 }}
+                        animate={{ x: '-100%' }}
+                        transition={{ repeat: Infinity, duration: 5, ease: 'linear' }}
                     >
                         {text}
-                    </h1>
+                    </motion.h1>
                 ))}
             </div>
         </div>
